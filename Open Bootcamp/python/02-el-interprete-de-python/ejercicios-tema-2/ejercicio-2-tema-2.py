45f3075306d05d1b8c1a9e85fe27c5d20d51948f
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2022-05-17 23:02:20 +0200

    Add files via upload

---
+++ Open Bootcamp/python/02-el-interprete-de-python/ejercicios-tema-2/ejercicio-2-tema-2.py
@@ -0,0 +1,7 @@
+'''
+Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+'''
+
+variable = "Ciao mundo!"
+print(variable)
